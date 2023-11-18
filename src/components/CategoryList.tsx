52701f5ff71782e0b48ad2003ee9f25ce8fd7165
moein <moein.bannaye.zahmati@gmail.com>
2023-11-18 20:12:48 +0330

    implement categoty list component

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,3 +1,95 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function CategoryList() {
-  return <div>CategoryList</div>;
+  return (
+    <div className="">
+      <h1 className="my-12 text-3xl font-semibold">Popular Categories</h1>
+      <div className="flex flex-wrap justify-between items gap-x-5">
+        <Link
+          href="/blog?cat=style"
+          className="flex items-center justify-center gap-3 capitalize w-[15%] h-20 rounded-xl text-slate-900 bg-sky-200"
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            width={32}
+            height={32}
+            className="rounded-xl"
+          />
+          style
+        </Link>
+
+        <Link
+          href="/blog?cat=fashion"
+          className="flex items-center justify-center gap-3 capitalize w-[15%] h-20 rounded-xl text-slate-900 bg-pink-200"
+        >
+          <Image
+            src="/fashion.png"
+            alt=""
+            width={32}
+            height={32}
+            className="rounded-xl"
+          />
+          fashion
+        </Link>
+
+        <Link
+          href="/blog?cat=food"
+          className="flex items-center justify-center gap-3 capitalize w-[15%] h-20 rounded-xl text-slate-900 bg-green-200"
+        >
+          <Image
+            src="/food.png"
+            alt=""
+            width={32}
+            height={32}
+            className="rounded-xl"
+          />
+          food
+        </Link>
+
+        <Link
+          href="/blog?cat=travel"
+          className="flex items-center justify-center gap-3 capitalize w-[15%] h-20 rounded-xl text-slate-900 bg-purple-200"
+        >
+          <Image
+            src="/travel.png"
+            alt=""
+            width={32}
+            height={32}
+            className="rounded-xl"
+          />
+          travel
+        </Link>
+
+        <Link
+          href="/blog?cat=culture"
+          className="flex items-center justify-center gap-3 capitalize w-[15%] h-20 rounded-xl text-slate-900 bg-orange-200"
+        >
+          <Image
+            src="/culture.png"
+            alt=""
+            width={32}
+            height={32}
+            className="rounded-xl"
+          />
+          culture
+        </Link>
+
+        <Link
+          href="/blog?cat=coding"
+          className="flex items-center justify-center gap-3 capitalize w-[15%] h-20 rounded-xl text-slate-900 bg-red-200"
+        >
+          <Image
+            src="/coding.png"
+            alt=""
+            width={32}
+            height={32}
+            className="rounded-xl"
+          />
+          coding
+        </Link>
+      </div>
+    </div>
+  );
 }
